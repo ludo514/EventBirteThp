c81200c889085003b22c92b312ca792773208df7
ludo514 <ludovict40@gmail.com>
2019-11-04 15:26:28 +0100

    ajout seed et validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,15 +2,14 @@ class Event < ApplicationRecord
 	has_many :attendances
 	has_many :users, through: :attendance
 
-	validates :start_date, presence: true,
-	if: :is_gone?
+	validates :start_date, presence: true
+	validate :is_gone?
 
-	validates :duration, presence: true,
-	numericality: { greater_than: 0 },
-	if: :duration_5?
+	validates :duration,presence: true, numericality: { greater_than: 0 }
+	validate :duration_5?
+	
 
-	validates :title,
-	presence: true,
+	validates :title, presence: true,
 	length: {minimum: 5, maximum: 140}
 
 	validates :description, presence: true,
@@ -22,9 +21,13 @@ class Event < ApplicationRecord
 	validates :location, presence: true
 
 	def duration_5?
-		return self.duration % 5 == 0
+		errors.add(:duration, "La durée doit être un multiple de 5") unless
+		self.duration % 5 == 0 
 	end
+
 	def is_gone?
-		return self.start_date > DateTime.now
+		errors.add(:start_date, "La date doit être supérieur")unless 
+		self.start_date > DateTime.now 
 	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+u = []
+e = []
+
+10.times do |index|
+	u << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10),email: Faker::Name.first_name + "@yopmail.com", encrypted_password:"ludo")
+	puts "User : #{Faker::Name.first_name}"
+end
+
+10.times do |index|
+	e << Event.create(start_date: Faker::Time.forward(days: 28), duration: 50, description: Faker::Lorem.sentence(word_count: 10),title: Faker::Book.title, location:"Paris",price: rand(1..1000))
+	puts "Event : #{Faker::Time.forward(days: 28)}"
+end
+
+10.times do |index|
+	Attendance.create(user: u.sample, event: e.sample)
+	puts "Participation crée"
+end
