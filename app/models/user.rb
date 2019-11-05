6aad7dc12091849b2b3c5bbce9d4c853718dccab
ludo514 <ludovict40@gmail.com>
2019-11-05 23:00:42 +0100

    ajout envoie mail créa user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
 	has_many :events, through: :attendance
 	has_many :administrator_events, foreign_key: 'administrator_id', class_name: "Event"
 	
-	#after_create :welcome_send
+	after_create :welcome_send
 
-  	#def welcome_send  
-    	#UserMailer.welcome_email(self).deliver_now
-  	#end
+  	def welcome_send  
+    	UserMailer.welcome_email(self).deliver_now
+  	end
 end
